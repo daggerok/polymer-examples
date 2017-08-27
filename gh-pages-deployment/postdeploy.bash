5ea4de544988f97446721cc05e074f5497d5631e
Maksim Kostromin <daggerok@gmail.com>
2017-08-27 22:16:04 +0300

    Github Pages 2017-08-27 deployment

--- gh-pages-deployment/postdeploy.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# replace base-path with root:
-# /polymer-examples/ -> /
-
-if ! [ -z "$1" ]; then
-  sed -i -e "s/\<head\>\<base\ href=\"\/polymer-examples\/\"\/>/\<head\>/g" $1
-fi
--- gh-pages-deployment/predeploy.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# add base tag:
-# <head> -> <head><base href="/polymer-examples/"/>
-
-if ! [ -z "$1" ]; then
-  sed -i -e "s/\(<head>\)/<head><base href=\"\/polymer-examples\/\"\/>/g" $1
-fi
