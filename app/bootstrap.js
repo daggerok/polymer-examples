cfebee9966e8a3778d2073b8a2f6f3c86b1cd71e
Maksim Kostromin <daggerok@gmail.com>
2017-09-29 21:00:51 +0300

    Github Pages 2017-09-29 deployment

---
+++ app/bootstrap.js
@@ -0,0 +1,39 @@
+var webComponentsAreSupported =
+  ('registerElement' in document)
+  && ('import' in document.createElement('link'))
+  && ('content' in document.createElement('template'));
+
+console.log('webComponentsAreSupported', webComponentsAreSupported);
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOMContentLoaded');
+});
+
+document.addEventListener('load', () => {
+  console.log('load');
+});
+
+// app main entry point must be here:
+function finalizeLazyLoading() {
+  console.log('bootstrapping app...');
+
+  var appHeaderLayout = document.querySelector('app-header-layout[unresolved=""]');
+  if (appHeaderLayout.hasAttribute('unresolved')) delete appHeaderLayout.removeAttribute('unresolved');
+
+  console.log('app bootstrapped');
+}
+
+if (!webComponentsAreSupported) {
+
+  var script = document.createElement('script');
+
+  script.async = true;
+  script.src = './bower_components/webcomponentsjs/webcomponents-loader.js';
+  script.onload = finalizeLazyLoading;
+  document.head.appendChild(script);
+  console.log('polyfills where added.');
+
+} else {
+
+  finalizeLazyLoading();
+}
